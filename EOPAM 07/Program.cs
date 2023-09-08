4b1167b85a010501d3689848ef7ee9ca9af73b13
TobiTobiT0bi <106669183+TobiTobiT0bi@users.noreply.github.com>
2023-09-08 16:05:59 -0300

    eopam 08 y eopam 07

---
+++ EOPAM 07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOPAM_07
+{
+    public class Raices {
+        public int A = 0;
+        public int B = 0;
+        public int C = 0;
+
+        public Raices(int a, int b, int c) { 
+            this.A = a; this.B = b; this.C = c;
+        }
+
+        public void obtenerRaices() { /*
+            int resultadoMas = Convert.ToInt32((-1 * B + Math.Sqrt(B * B - 4 * A * C)) / 2 * A);
+            int resultadoMenos = Convert.ToInt32((-1 * B - Math.Sqrt(B * B - 4 * A * C)) / 2 * A);
+
+            Console.WriteLine($"Raiz 1: {resultadoMas}, raiz 2: {resultadoMenos}");*/
+
+            Console.WriteLine($"Raiz 1: {Convert.ToInt32((-1 * B + Math.Sqrt(B * B - 4 * A * C)) / 2 * A)}, raiz 2: {Convert.ToInt32((-1 * B - Math.Sqrt(B * B - 4 * A * C)) / 2 * A)}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ EOPAM 08/profesores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOPAM_08
+{
+    internal class Profesores : Persona
+    {
+        public string materia = "";
+
+        string[] materiasDisponibles = { "matematicas", "filosofia", "fisica" };
+
+        public Profesores(Random rnd) : base(rnd)
+        {
+            materia = materiasDisponibles[rnd.Next(0, materiasDisponibles.Length)];
+            edad = rnd.Next(25, 60);
+        }
+
+        public bool Vino(Random rnd)
+        {
+            esta = (rnd.Next(0, 5) == 4);
+            return esta;
+        }
+    }
+
+
+}
